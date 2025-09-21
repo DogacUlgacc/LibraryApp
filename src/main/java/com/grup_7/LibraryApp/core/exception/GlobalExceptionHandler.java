b4b9b3b7b0feb8c2d3e0270ecf0857b6bbb332f6
Seçil Keremoğlu <secilkeremoglu@gmail.com>
2025-09-21 20:22:33 +0300

    reservation update

--- src/main/java/com/grup_7/LibraryApp/core/exception/GlobalExceptionHandler.java
+++ src/main/java/com/grup_7/LibraryApp/core/exception/GlobalExceptionHandler.java
@@ -1,9 +1,22 @@
 package com.grup_7.LibraryApp.core.exception;
 
+import com.grup_7.LibraryApp.core.exception.type.BusinessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<String> handleBusinessException(BusinessException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleOtherExceptions(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Bir hata oluştu: " + ex.getMessage());
+    }
 
 }
